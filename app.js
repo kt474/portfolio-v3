ecbb59b28bc0dc9e7a89cbb4fbd25c1f7e6ee510
Kevin Tian <kt474@cornell.edu>
2021-01-06 23:50:02 -0500

    adding particle js animation

--- app.js
+++ app.js
@@ -16,3 +16,8 @@ $(document).ready(function ($) {
     document.getElementById("website").innerHTML = "website";
   }
 });
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load("particles-js", "assets/particles.json", function () {
+  console.log("callback - particles.js config loaded");
+});
